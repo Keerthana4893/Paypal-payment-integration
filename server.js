125861f7db494d07e2526aa509969e7f51eea5ef
Keerthana4893 <40628438+Keerthana4893@users.noreply.github.com>
2020-02-16 01:43:50 +0530

    db params

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const config = require('./config')
+const config = require('./config');
 const payment = require('./lib/payment');
 const db  = require('./lib/db');
 const bodyParser = require('body-parser');
